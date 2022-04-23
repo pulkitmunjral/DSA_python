da22702a83d20713f06fe99b48414a512a9a6807
qe13026445 <qe13026445@ldap.com>
2022-04-23 12:55:37 +0530

    leetcode que

--- Amazon/141.linked-list-cycle.py
+++ Amazon/141.linked-list-cycle.py
@@ -6,10 +6,10 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
