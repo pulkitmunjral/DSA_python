c54615d5773f9a73690a3790f87e185a5be568b7
qe13026445 <qe13026445@ldap.com>
2022-04-24 17:42:48 +0530

    infosys

---
+++ Other/divisionGame.py
@@ -0,0 +1,14 @@
+# not yet completed
+
+
+even = -1
+odd = 2
+
+value = 8
+even_ans = 0
+odd_ans = 0
+while value>1:
+    if even>odd and value%2==0:
+        even_ans+=1
+        value/=2
+
--- Other/swapInfosys.py
+++ Other/swapInfosys.py
@@ -1,5 +1,7 @@
-array_1 = [1, 2]
-array_2 = [1, 2]
+# not yet your complete worst case working fine
+
+array_1 = [3, 2, 1]
+array_2 = [1, 2, 3]
 
 i = 0
 ans = 0
@@ -12,7 +14,7 @@ while array_2 != array_1:
         ans += array_1[i] - array_1[i + 1]
         array_1[i], array_1[i+1] = array_1[i+1], array_1[i]
         i += 1
-    if i == len(array_1):
+    if i == len(array_1) - 1:
         i = 0
     ans = ans % mod
 
