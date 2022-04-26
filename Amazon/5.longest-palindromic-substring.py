3aa05bc2cf3e3cd5e1e17abc7004153a9d176b89
qe13026445 <qe13026445@ldap.com>
2022-04-26 19:39:41 +0530

    longest palandromic substring

---
+++ Amazon/5.longest-palindromic-substring.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=5 lang=python3
+#
+# [5] Longest Palindromic Substring
+#
+
+# @lc code=start
+from os import pread
+import re
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s)<2:
+            return s
+        i = 0 
+        temp = s[0]
+        max = ""
+        for i in range(len(s)):
+            j=len(s)-1
+            while(i<j):
+                if s[i]==s[j]:
+                    temp = s[i:j+1]
+                    if temp == temp[::-1]:
+                        break
+                    else:
+                        temp = s[0]
+                j-=1
+
+            if len(temp)>len(max):
+                max = temp
+        return max
+                
+        
+# @lc code=end
+
