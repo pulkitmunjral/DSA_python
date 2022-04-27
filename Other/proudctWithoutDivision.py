6b9b4f17a180486cd939255b0568904c5b1bd28f
qe13026445 <qe13026445@ldap.com>
2022-04-27 18:03:09 +0530

    product of list without /

---
+++ Other/proudctWithoutDivision.py
@@ -0,0 +1,18 @@
+# find product of list of element other than self without using division
+
+
+def product(array):
+    ans = []
+    temp = 1
+    for i in array:
+        ans.append(temp)
+        temp *= i
+
+    temp = 1
+    for j in range(len(array)-1, -1, -1):
+        ans[j] *= temp
+        temp *= array[j]
+    return ans
+
+arr = [-1,1,0,-3,3]
+print(product(arr))
