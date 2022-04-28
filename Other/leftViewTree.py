d2e2a545f5857f0807f8aaafe3273381d1c64fcc
qe13026445 <qe13026445@ldap.com>
2022-04-28 23:02:28 +0530

    left view of tree

---
+++ Other/leftViewTree.py
@@ -0,0 +1,34 @@
+# Print the element which is visible from left view
+# we can keep track of level and print the first element of that level
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+
+global maxLevel
+maxLevel = -1
+
+
+def leftViewTree(root, level):
+
+    global maxLevel
+    if not root:
+        return
+
+    if level > maxLevel:
+        print(root.value)
+        maxLevel = level
+
+    leftViewTree(root.left, level+1)
+    leftViewTree(root.right, level+1)
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.right = Node(5)
+
+
+leftViewTree(root,0)
