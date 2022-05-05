78fd03117eda150c4e125ab1643fe448a9c271a5
qe13026445 <qe13026445@ldap.com>
2022-05-05 21:13:30 +0530

    merge k sorted lists

--- Amazon/23.merge-k-sorted-lists.py
+++ Amazon/23.merge-k-sorted-lists.py
@@ -6,10 +6,10 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
@@ -17,17 +17,17 @@ class Solution:
         amount = len(lists)
         interval = 1
 
-        while interval<amount:
-            for i in range(0,amount-interval,interval*2):
-                lists[i] = self.merge2(lists[i],lists[i+interval])
-            interval*=2
-        return lists[0] if amount>0 else None
+        while interval < amount:
+            for i in range(0, amount-interval, interval*2):
+                lists[i] = self.merge2(lists[i], lists[i+interval])
+            interval *= 2
+        return lists[0] if amount > 0 else None
 
     def merge2(self, l1, l2):
         head = pointer = ListNode(0)
 
         while l1 and l2:
-            if l1.val<=l2.val:
+            if l1.val <= l2.val:
                 pointer.next = l1
                 l1 = l1.next
             else:
