a63ef5671780f9c05c369f220030f9a4460b0401
qe13026445 <qe13026445@ldap.com>
2022-05-07 12:36:52 +0530

    trapping rain water

--- Amazon/42.trapping-rain-water.py
+++ Amazon/42.trapping-rain-water.py
@@ -9,24 +9,31 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         waterTrapped = 0
         lenght = len(height)
+
         if lenght<1:
             return waterTrapped
+
         leftIndex = 0
         rightIndex = lenght-1
         leftMax = 0
         rightMax = 0
         
-        while leftIndex<rightIndex:
+        while leftIndex < rightIndex:
+
             leftHeight = height[leftIndex]
             rightHeight = height[rightIndex]
+
             if leftHeight < rightHeight:
                 if leftMax<leftHeight:
                     leftMax = leftHeight
+
                 leftIndex+=1
                 waterTrapped += (leftMax-leftHeight)
+
             else:
                 if rightMax<rightHeight:
                     rightMax = rightHeight
+                    
                 waterTrapped += (rightMax-rightHeight)
                 rightIndex-=1
         return waterTrapped
