efd97b79dcca1debd704e9319ec8f46787e7a35b
qe13026445 <qe13026445@ldap.com>
2022-05-08 11:29:10 +0530

    rotate array

---
+++ Amazon/189.rotate-array.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=189 lang=python3
+#
+# [189] Rotate Array
+#
+
+# @lc code=start
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
+        
+# @lc code=end
+
