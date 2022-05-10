f9fb965ec124ff2e39332a660bb553b9a6df0aca
qe13026445 <qe13026445@ldap.com>
2022-05-10 21:12:30 +0530

    repeated sub string

---
+++ Amazon/459.repeated-substring-pattern.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=459 lang=python3
+#
+# [459] Repeated Substring Pattern
+#
+
+# @lc code=start
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in (s+s)[1: -1]
+        
+# @lc code=end
+
