16a532b40c9c79debc7d883adec0e4b4be98d98f
qe13026445 <qe13026445@ldap.com>
2022-05-19 22:08:57 +0530

    back tracking

--- Other/Backtracking.py
+++ Other/Backtracking.py
@@ -7,20 +7,20 @@ def move(grid, visited, x, y, targetX, targetY, m, n):
         return inf
 
     if x == targetX and y == targetY:
-        return 1
+        return 0
     visited[x][y] = 1
     moveRight = move(grid, visited, x, y + 1, targetX, targetY, m, n)
     moveDown = move(grid, visited, x + 1, y, targetX, targetY, m, n)
     moveLeft = move(grid, visited, x, y - 1, targetX, targetY, m, n)
     moveUp = move(grid, visited, x - 1, y, targetX, targetY, m, n)
-    visited[x][y] = 0
+    # visited[x][y] = 0
     return 1 + min(moveRight, moveLeft, moveUp, moveDown)
 
 
-grid = [[0, 1, 0],
-        [0, 0, 1],
-        [1, 1, 1]]
+grid = [[1, 1, 1],
+        [1, 1, 1],
+        [1, 0, 1]]
 
 visited = [[False for _ in range(3)] for _ in range(3)]
-print(move(grid, visited, 2, 0, 1, 1, 3, 3))
+print(move(grid, visited, 2, 0, 0, 2, 3, 3))
 
